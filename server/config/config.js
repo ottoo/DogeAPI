1097137bc518124f675c151ced2a06f6587ddfcc
Otto Kivikärki <ottoki@gmail.com>
2016-06-21 19:28:53 +0300

    Get json data with google supported wgs84 instead

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  DOG_PARKS_URL: 'http://opendata.navici.com/tampere/opendata/ows?service=WFS&version=2.0.0&request=GetFeature&typeName=opendata:WFS_KOIRAPUISTO_MVIEW&outputFormat=json',
-  DOG_TRASH_BINS_URL: 'http://opendata.navici.com/tampere/opendata/ows?service=WFS&version=2.0.0&request=GetFeature&typeName=opendata:WFS_ROSKIS&outputFormat=json'
+  DOG_PARKS_URL: 'http://opendata.navici.com/tampere/opendata/ows?service=WFS&version=2.0.0&request=GetFeature&typeName=opendata:WFS_KOIRAPUISTO_MVIEW&outputFormat=json&srsName=EPSG:4326',
+  DOG_TRASH_BINS_URL: 'http://opendata.navici.com/tampere/opendata/ows?service=WFS&version=2.0.0&request=GetFeature&typeName=opendata:WFS_ROSKIS&outputFormat=json&srsName=EPSG:4326'
 };
